b75ec829ec8a4276d884df1788331651d85e82d7
thuquynhng <thuquynhng.neu@gmail.com>
2024-06-03 18:47:28 +0700

    Create DAY11_Exercises.sql

---
+++ DAY11_Exercises.sql
@@ -0,0 +1,46 @@
+--Exercise 1
+select COUNTRY.Continent, floor(avg(CITY.Population))
+from city as CITY
+inner join country as COUNTRY
+on CITY.CountryCode=COUNTRY.Code
+group by COUNTRY.Continent
+--Exercise 2
+SELECT round(count(B.email_id)::decimal/count(distinct A.email_id), 2) as activation_rate
+FROM emails as A
+left join texts as B
+on A.email_id=B.email_id
+and B.signup_action='Confirmed'
+--Exercise 3
+with snaps_timespending as (
+SELECT B.age_bucket,
+sum(case 
+	when A.activity_type ='send' then A.time_spent else 0
+end) as send,
+sum(case 
+	when A.activity_type ='open' then A.time_spent else 0
+end) as open,
+sum(A.time_spent) as total_timespent
+FROM activities as A
+inner join age_breakdown as B
+on A.user_id=B.user_id
+where A.activity_type in ('send', 'open')
+group by B.age_bucket
+)
+
+select age_bucket,
+round((send/total_timespent)*100.0, 2) as send_perc,
+round((open/total_timespent)*100.0, 2) as open_perc
+from snaps_timespending
+--Exercise 4
+with supercloud_customer as (
+SELECT a.customer_id, count(distinct b.product_category) as product	
+FROM customer_contracts as a
+left join products as b
+on a.product_id=b.product_id
+group by a.customer_id
+)
+select customer_id from supercloud_customer
+where product= (select count(distinct product_category) 
+from b)
+order by customer_id
+--Exercise 5
