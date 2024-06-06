2d7978f35c72e5802e0e0e5600e89c27dda7fb62
thuquynhng <thuquynhng.neu@gmail.com>
2024-06-06 22:56:53 +0700

    Update DAY13_Exercises.sql

--- DAY13_Exercises.sql
+++ DAY13_Exercises.sql
@@ -25,7 +25,7 @@ where page_id not in
 (select page_id from page_likes as B
 where A.page_id=B.page_id) --xem bảng A có id nào ko trùng với bảng B hay ko, đó là những trang ko có lượt like
 ;
---Exercise 5
+--Exercise 5 (*)
 with july as (
 SELECT user_id,
 count(user_id),
@@ -65,3 +65,42 @@ on A.product_id=B.product_id
 and A.year=B.minyear
 ;
 --Exercise 8
+select customer_id from Customer
+group by customer_id
+having count(distinct product_key) = (select count(*) from Product)
+;
+--Exercise 9
+select emp.employee_id
+from Employees as emp
+left join Employees as mng on emp.manager_id= mng.employee_id
+where emp.manager_id not in 
+(select employee_id from Employees)
+and emp.salary <30000
+order by emp.employee_id 
+;
+--Exercise 11
+(select Users.name as results from Users 
+join MovieRating on Users.user_id=MovieRating.user_id
+group by Users.name
+order by count(MovieRating.movie_id) desc, Users.name asc
+limit 1)
+union all
+(select Movies.title as results from Movies 
+join MovieRating on Movies.movie_id=MovieRating.movie_id
+where MovieRating.created_at between '2020-02-01' and '2020-02-29'
+group by Movies.title
+order by avg(MovieRating.rating) desc, Movies.title asc
+limit 1)
+;
+--Exercise 12
+with friends_count as (
+select requester_id as id 
+from RequestAccepted
+union all 
+select accepter_id as id
+from RequestAccepted) --list tất cả các người dùng kể cả requesters và accepters
+select id, count(*) as num from friends_count --đếm số bạn bè 
+group by id
+order by count(*) desc
+limit 1
+
